7786e99c1597815065e20e850850abffe0888dd9
Ricky Domingos <ricardodomingos97@gmail.com>
2020-08-28 22:54:00 -0300

    const express added to server.js to access the home page

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
-require('express')()
+const express = require('express')
+const server = express()
+
+server.use(express.static("public"))
+
 .get("/", (req, res) => {
-    return res.send("Hi from NLW")
+    return res.sendFile(__dirname + "/views/index.html")
 })
 .get("/study", (req, res) => {
     return res.send("Pagina Study! seja bem vindo")
 })
+
 .listen(5500)
