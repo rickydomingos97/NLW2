685df2b600e0605a23cb7454ea57b5e16d9a3175
Ricky Domingos <ricardodomingos97@gmail.com>
2020-09-20 11:40:24 -0300

    created database/fake_data.js and /src/utils/format.js

---
+++ src/utils/format.js
@@ -0,0 +1,38 @@
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",                          
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",            
+]
+// com o caminho dos arquivos ja foi indicado no nunjucks, podemos remover o __dirname + o caminho e apenas pedir para renderizar o arquivo na function logo abaixo:
+// BEFORE: return res.render(__dirname + "/views/index.html")
+// AFTER: 
+
+// FUNCTIONS:
+function getSubject (subjectNumber) {
+    // o -1 foi criado para poder selecionar o numero do array do subject corretamente.
+    const position  = +subjectNumber - 1
+    return subjects[position]
+}
+
+module.exports = {
+    subjects,
+    weekdays,
+    getSubject
+}
