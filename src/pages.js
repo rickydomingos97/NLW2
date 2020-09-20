905ca611a65b4dc6d1bd97612e887ca2f710a0c3
Ricky Domingos <ricardodomingos97@gmail.com>
2020-09-20 12:41:53 -0300

    created src/pages.js and refactoring server.js file

---
+++ src/pages.js
@@ -0,0 +1,34 @@
+const { subjects, weekdays, getSubject } = require('./utils/format.js')
+
+function pageLanding(req, res) {
+    return res.render("index.html")
+}
+// renderizando objeto dentro do render: passando objetos para dentro da function
+function pageStudy(req, res) {
+    const filters = req.query
+    return res.render("study.html", { proffys, filters, subjects, weekdays })
+}
+
+function pageGiveClasses(req, res) {
+    const data = req.query
+       
+    // if exist data:
+    const isNotEmpty = Object.keys(data).length > 0
+    if (isNotEmpty) {
+
+        data.subject = getSubject(data.subject)
+   
+        // add data to proffys array list
+        proffys.push(data)
+        // if all of data is ok, you will get redirection to study page with this command:
+        return res.redirect("/study")//you will be redirected to study page
+    }
+    // if not exist any data, to show the page
+    return res.render("give-classes.html", {subjects, weekdays})
+}
+
+module.exports = {
+    pageLanding,
+    pageStudy,
+    pageGiveClasses
+}
--- src/server.js
+++ src/server.js
@@ -1,40 +1,11 @@
-function pageLanding(req, res) {
-    return res.render("index.html")
-}
-// renderizando objeto dentro do render:
-//passando objetos para dentro da function
-function pageStudy(req, res) {
-    const filters = req.query
-    return res.render("study.html", { proffys, filters, subjects, weekdays
-    })
-}
-function pageGiveClasses(req, res) {
-    const data = req.query
-    
-    const isNotEmpty = Object.keys(data).length > 0
-    // if exist data:
-    if (isNotEmpty) {
-
-        data.subject = getSubject(data.subject)
-        
-        // add data to proffys array list
-        proffys.push(data)
-        // if all of data is ok, you will get redirection to study page with this command:
-        return res.redirect("/study")//you will be redirected to study page
-    }
-    // if not exist any data, to show the page
-    return res.render("give-classes.html", {subjects, weekdays})
-}
-
 // SERVER:
-
 // importando o express
 const express = require('express')
 const server = express() // esse server nos retorna um objeto
+
+const {pageLanding, pageStudy, pageGiveClasses } = require('./pages')
 //importando o nunjucks
-
 // TEMPLATE ENGINE:
-
 const nunjucks = require('nunjucks')
 // configurar nnjucks
 // dizer ao nunjucks onde estao os arquivos html
