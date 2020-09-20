6e4fc3e284d67c9552838f2f72af048325b3f1e2
Ricky Domingos <ricardodomingos97@gmail.com>
2020-09-20 14:08:20 -0300

    try catch error configured and working on pages.js

--- src/pages.js
+++ src/pages.js
@@ -6,7 +6,7 @@ function pageLanding(req, res) {
     return res.render("index.html")
 }
 // renderizando objeto dentro do render: passando objetos para dentro da function
-function pageStudy(req, res) {
+async function pageStudy(req, res) {
     const filters = req.query
 
     if (!filters.subject || !filters.weekday || !filters.time) {
@@ -19,16 +19,28 @@ function pageStudy(req, res) {
     const query = `
         SELECT classes.*, proffys.*
         FROM proffys
-        JOIN classes ON (classes.proffy_id = proffy_id)
+        JOIN classes ON (classes.proffy_id = proffys.id)
         WHERE EXISTS (
             SELECT class_schedule.*
             FROM class_schedule
             WHERE class_schedule.class_id = classes.id
-            AND class_schedule.weekday = ${filters.weekdays}
+            AND class_schedule.weekday = ${filters.weekday}
             AND class_schedule.time_from <= ${timeToMinutes}
             AND class_schedule.time_to > ${timeToMinutes}
         )
-    `    
+        AND classes.subject = '${filters.subject}'
+    `
+
+    // caso haja erros na consulta do banco de dados
+    try {
+        const db = await Database
+        const proffys = await db.all(query)
+
+        return res.render('study.html', { proffys, subjects, filters, weekdays })
+
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 function pageGiveClasses(req, res) {
